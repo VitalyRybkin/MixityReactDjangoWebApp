73ebccd6a11651272cb8c4fe240dcdecab4fb8cf
vitaly <vitaly.rybkin@gmail.com>
2026-01-22 20:16:22 +0300

    feat(docs): integrate Swagger UI and static file support
    - Added Swagger UI integration for API documentation under `/docs/` route.
    - Configured static file handling for serving Swagger UI assets.
    - Updated `settings.py` to include static file directories and Swagger-specific templates.
    - Enhanced project structure with `swagger_ui` directory for custom assets and styling.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = "backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "swagger_ui/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -151,6 +151,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "static"
+
+STATICFILES_DIRS = [BASE_DIR / "swagger_ui/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,12 @@
+import os
+
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.views.static import serve
+from django.conf.urls.static import static
+
+from backend import settings
 from catalog.views import UserCreateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -10,4 +17,6 @@ urlpatterns = [
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("auth/auth/", include("rest_framework.urls")),
     path("api/", include("carrier.urls")),
-]
+    path("docs/", TemplateView.as_view(template_name="swagger-ui.html"), name="swagger-ui"),
+    path("swagger/<path:path>", serve, {"document_root": os.path.join(settings.BASE_DIR, "swagger")}),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
