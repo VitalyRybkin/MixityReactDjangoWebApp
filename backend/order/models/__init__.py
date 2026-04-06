3b26c640b20d283ba6245430b4f031b8f8549181
vitaliyrybkin <vitaliy.rybkin@yandex.ru>
2026-04-06 20:18:28 +0300

    feat(backend): add `Order` model to `order` module
    - Implement `Order` model with fields for status, client, customer, and related metadata.
    - Define `Status` choices for order lifecycle management.
    - Add `Order` to `__all__` in `order.models` for module exports.

--- backend/order/models/__init__.py
+++ backend/order/models/__init__.py
@@ -1,5 +1,6 @@
 from .client import Client
 from .construction_object import ConstructionObject
 from .customer import Customer
+from .order import Order
 
-__all__ = ["Client", "Customer", "ConstructionObject"]
+__all__ = ["Client", "Customer", "ConstructionObject", "Order"]
---
+++ backend/order/models/order.py
@@ -0,0 +1,58 @@
+from django.conf import settings
+from django.db import models
+
+
+class Order(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "draft", "Черновик"
+        CREATED = "created", "Создан"
+        IN_PROGRESS = "in_progress", "В процессе"
+        COMPLETED = "completed", "Завершен"
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    delivery_date = models.DateField(
+        null=True, blank=True, verbose_name="Дата доставки"
+    )
+
+    client = models.ForeignKey(
+        "order.Client",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Клиент",
+    )
+    customer = models.ForeignKey(
+        "order.Customer",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Заказчик",
+    )
+    customer_object = models.ForeignKey(
+        "order.ConstructionObject",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Объект",
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.DRAFT,
+        verbose_name="Статус заказа",
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Кто создал",
+    )
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+        ordering = ["-created_at"]
